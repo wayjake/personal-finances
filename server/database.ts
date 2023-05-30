3e2489bcb415cedf37682dc5298a4129d4a15fbc
Jake <jake@xyzdigital.com>
2023-05-30 09:59:14 -0700

    prettier cleanup

--- server/database.ts
+++ server/database.ts
@@ -1,6 +1,6 @@
-import { Database } from "bun:sqlite";
-const db = new Database("mydb.sqlite", { create: true });
+import { Database } from 'bun:sqlite'
+export const db = new Database('mydb.sqlite', { create: true })
 
 export const close = async () => {
-  db.close();
-};
+    db.close()
+}
